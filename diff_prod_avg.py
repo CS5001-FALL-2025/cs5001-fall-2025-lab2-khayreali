c3de0de890bc3062631255cf5e6129b3a2a3d712
Khayre <khayreahmed@gmail.com>
2025-09-10 15:59:15 -0400

    completed diff_prod_avg

--- diff_prod_avg.py
+++ diff_prod_avg.py
@@ -22,14 +22,14 @@ def main():
     y = 10
 
     # Assign values to the variables below to calculate the difference, product, and average of the values in x and y.
-    difference = # YOUR CODE HERE
-    product = # YOUR CODE HERE
-    average = # YOUR CODE HERE
+    difference = y - x
+    product = x * y
+    average = (x + y) / 2
 
     # Replace the # YOUR CODE HERE text in the statement below so that the statement will print
-    # The difference between x and y is difference.
-    # where x, y, and difference are replaced with the values stored in the corresponding variables.    
-    print (f'# YOUR CODE HERE')
+    print (f"The difference between x and y is {difference}")
+    print (f"The product of x and y is {product}")
+    print (f"The average of x and y is {average}")
     # Write a statement to print the product of x and y.
     # Write a statement to print the average of x and y. Make sure this prints 7.5!
 
