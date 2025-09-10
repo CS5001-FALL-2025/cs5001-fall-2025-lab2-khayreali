9022149c20b575d142e38610e785b6401befe091
Khayre <khayreahmed@gmail.com>
2025-09-10 16:06:06 -0400

    completed song_lyrics

--- song_lyrics.py
+++ song_lyrics.py
@@ -12,7 +12,19 @@ lyric on one single line using print.
 '''
 
 def main():
-    # YOUR CODE HERE
+    # https://genius.com/Daft-punk-harder-better-faster-stronger-lyrics
+    word_1 = "work"
+    word_2 = "it"
+    word_3 = "make"
+    word_4 = "it"
+    word_5 = "Do"
+    word_6 = "it"
+    word_7 = "makes"
+    word_8 = "us"
+    word_9 = "Harder"
+    word_10 = "better"
+
+    print(word_1, word_2, word_3, word_4, word_5, word_6, word_7, word_8, word_9, word_10)
 
 
 if __name__ == '__main__':
